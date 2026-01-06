bad0873370ce774552a6e5e0f511f1e157c77b00
Ubuntu <ubuntu@vps-8c3ed8e0.vps.ovh.ca>
2026-01-06 02:52:15 +0000

    Skip eslint during builds

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     unoptimized: true,
   },
